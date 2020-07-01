3e29967e3fb37805439bb0f1c2747f9660005186
Bùi Việt Thành <thanhbv@sandinh.net>
2020-07-02 01:53:36 +0700

    Add ParensAroundLambda rule

---
+++ output/src/test/scala/fix/scala213/ParensAroundLambda.scala
@@ -0,0 +1,17 @@
+package fix.scala213
+
+abstract class ParensAroundLambda {
+  Nil.foreach { (x: Nothing) => } // fix
+  Nil.foreach { (x: Nothing) => } // keep
+  val f: String => Unit = (s: String) => () // keep
+  Map("a" -> 1).map { (x: (String, Int)) => // fix
+    ???
+  }
+
+  Seq(1).map { (i: Int) => // fix
+    i + 1
+  }
+  Seq(1).map { i => i + 1 } // keep
+
+  Seq(1).foreach { implicit i: Int => } // keep
+}
