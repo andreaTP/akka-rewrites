8aead178f2d81a330c3393f6f0a8703111429d29
Bùi Việt Thành <thanhbv@sandinh.net>
2020-07-09 23:12:01 +0700

    Fix NullaryOverride can't rewrite local def symbols

--- rewrites/src/main/scala/impl/NullaryPower.scala
+++ rewrites/src/main/scala/impl/NullaryPower.scala
@@ -1,10 +1,18 @@
 package impl
 
 import scalafix.internal.rule.CompilerException
+import scalafix.v1._
+
 import scala.annotation.tailrec
 import scala.meta.Tree
+import scala.meta.internal.pc.ScalafixGlobal
+import scala.meta.internal.proxy.GlobalProxy
 
-trait NullaryPower { this: impl.IPower =>
+trait NullaryPower {
+  val g: ScalafixGlobal
+  implicit val doc: SemanticDocument
+  private lazy val unit = g.newCompilationUnit(doc.input.text, doc.input.syntax)
+
   /** Similar to `nextOverriddenSymbol` but loop through ancestors.reverse
    * @see [[scala.reflect.internal.Symbols.Symbol.nextOverriddenSymbol]] */
   private def rootOverriddenSymbol(s: g.Symbol): g.Symbol = {
@@ -26,7 +34,15 @@ trait NullaryPower { this: impl.IPower =>
   }
 
   def isNullaryMethod(t: Tree): Option[Boolean] = try {
-    val meth = gsymbol(t)
+    val meth: g.Symbol = {
+      val sSym = t.symbol
+      if (sSym.isLocal) {
+        GlobalProxy.typedTreeAt(g, unit.position(t.pos.start)).symbol
+      } else {
+        val gSymbols = g.inverseSemanticdbSymbols(sSym.value)
+        gSymbols.find(gSym => g.semanticdbSymbol(gSym) == sSym.value).getOrElse(g.NoSymbol)
+      }
+    }
     val isJavaDefined = meth.overrideChain.exists(sym => sym.isJavaDefined || sym.owner == g.definitions.AnyClass)
 
     if (isJavaDefined) None
