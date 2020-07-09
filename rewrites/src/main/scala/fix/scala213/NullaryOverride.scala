f85d2801643c4fc321a3223bc39ce48942bbabc4
Bùi Việt Thành <thanhbv@sandinh.net>
2020-07-09 22:56:26 +0700

    NullaryOverride - Split code to separated files

--- rewrites/src/main/scala/fix/scala213/NullaryOverride.scala
+++ rewrites/src/main/scala/fix/scala213/NullaryOverride.scala
@@ -1,13 +1,6 @@
 package fix.scala213
 
-import java.io.FileWriter
-import java.nio.file.{Path, Paths}
-
-import scala.annotation.tailrec
-import scala.util.Using
-
 import metaconfig.Configured
-import scalafix.internal.rule.CompilerException
 import scalafix.internal.v1.LazyValue
 import scalafix.v1._
 
@@ -15,6 +8,7 @@ import scala.meta._
 import scala.meta.internal.pc.ScalafixGlobal
 import scala.meta.tokens.Token.{LeftParen, RightParen}
 
+import impl.{IPower, NullaryPower}
 import NullaryOverride._
 import NullaryOverrideMode._
 
@@ -49,7 +43,7 @@ class NullaryOverride(
   *       Similar, `t.foo _` must be rewritten to `() => t.foo` */
 object NullaryOverride {
   def collector(
-       power: => IPower,
+       power: => NullaryPower,
        config: NullaryOverrideConfig
   )(implicit doc: SemanticDocument): PartialFunction[Tree, Patch] = config.mode match {
     case ResetAndCollect | CollectAppend => {
@@ -91,95 +85,5 @@ object NullaryOverride {
   }
 
   final class Power(val g: ScalafixGlobal)(implicit val doc: SemanticDocument)
-      extends IPower with impl.IPower
-
-  trait IPower { this: impl.IPower =>
-    /** Similar to `nextOverriddenSymbol` but loop through ancestors.reverse
-      * @see [[scala.reflect.internal.Symbols.Symbol.nextOverriddenSymbol]] */
-    private def rootOverriddenSymbol(s: g.Symbol): g.Symbol = {
-      import g._, s._
-      @tailrec def loop(bases: List[Symbol]): Symbol = bases match {
-        case Nil          => NoSymbol
-        case base :: rest =>
-          val sym = overriddenSymbol(base)
-          if (sym == NoSymbol) loop(rest) else sym
-      }
-      if (isOverridingSymbol) loop(owner.ancestors.reverse) else NoSymbol
-    }
-    @tailrec private def isNullary(t: g.Type): Boolean = t match {
-      case g.NullaryMethodType(_) => true
-      case m: g.MethodType => m.isImplicit
-      case g.PolyType(_, r) => isNullary(r)
-      case _ => false
-    }
-
-    def isNullaryMethod(t: Tree): Option[Boolean] = try {
-      val meth = gsymbol(t)
-      val isJavaDefined = meth.overrideChain.exists(sym => sym.isJavaDefined || sym.owner == g.definitions.AnyClass)
-
-      if (isJavaDefined) None
-      else rootOverriddenSymbol(meth) match {
-        case m: g.MethodSymbol => Some(isNullary(m.info))
-        case _ => None
-      }
-    } catch {
-      case e: Throwable => throw CompilerException(e)
-    }
-  }
-}
-
-import scala.io.Source
-import scala.collection.mutable
-import metaconfig._
-import metaconfig.generic.Surface
-import scalafix.internal.config.ReaderUtil
-import NullaryOverrideConfig._
-
-case class NullaryOverrideConfig(
-    mode: NullaryOverrideMode = CollectAppend,
-    nullarySymPath: Path = Paths.get(".nullary.NullaryOverride"),
-    nonNullarySymPath: Path = Paths.get(".nonNullary.NullaryOverride")
-) {
-  val nullarySymbols: mutable.Set[String] = mutable.Set.empty
-  val nonNullarySymbols: mutable.Set[String] = mutable.Set.empty
-  lazy val nullaryMatcher: SymbolMatcher = matcher(nullarySymPath)
-  lazy val nonNullaryMatcher: SymbolMatcher = matcher(nonNullarySymPath)
-
-  def saveCollected(): Unit = mode match {
-    case ResetAndCollect =>
-      writeSymbols(nullarySymbols, nullarySymPath)
-      writeSymbols(nonNullarySymbols, nonNullarySymPath)
-    case CollectAppend =>
-      appendSymbols(nullarySymbols, nullarySymPath)
-      appendSymbols(nonNullarySymbols, nonNullarySymPath)
-    case Rewrite => // do nothing
-  }
-}
-
-object NullaryOverrideConfig {
-  val default: NullaryOverrideConfig = NullaryOverrideConfig()
-  implicit val reader: ConfDecoder[NullaryOverrideConfig] = generic.deriveDecoder[NullaryOverrideConfig](default)
-  implicit val surface: Surface[NullaryOverrideConfig] = generic.deriveSurface[NullaryOverrideConfig]
-
-  private def readLines(p: Path): List[String] =
-    Using(Source.fromFile(p.toFile))(_.getLines.toList).getOrElse(Nil)
-  private def writeSymbols(symbols: Iterable[String], p: Path): Unit =
-    Using(new FileWriter(p.toFile)) { fw =>
-      symbols.foreach { sym => fw.write(s"$sym\n") }
-    }.get
-  private def appendSymbols(symbols: Iterable[String], p: Path): Unit =
-    writeSymbols(readLines(p).toSet ++ symbols, p)
-
-  private def matcher(p: Path) = SymbolMatcher.exact(readLines(p): _*)
-}
-
-sealed trait NullaryOverrideMode
-object NullaryOverrideMode {
-  case object ResetAndCollect extends NullaryOverrideMode
-  case object CollectAppend extends NullaryOverrideMode
-  case object Rewrite extends NullaryOverrideMode
-
-  private def all = Seq(ResetAndCollect, CollectAppend, Rewrite)
-  implicit val reader: ConfDecoder[NullaryOverrideMode] =
-    ReaderUtil.fromMap(all.map(x => x.toString -> x).toMap)
+      extends NullaryPower with IPower
 }
